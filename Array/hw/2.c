ab9673d32186931c389b9aa102fdf6205f9398e0
mahfujarr <mahfujarrahmanjoy@gmail.com>
2024-04-24 20:49:33 +0600

    update

--- Array/hw/2.c
+++ Array/hw/2.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 #define row 5
 #define col 5
-#define rc 9
+#define rc (row-2)*(col-2)
 int main()
 {
     int result[rc];
+
     int arr[row][col]={{1,0,2,1,3},
                        {2,0,6,2,5},
                        {1,7,2,9,0},
                        {3,2,4,5,1},
                        {3,0,1,0,5}};
+
     for (int i = 0; i < row-2; i++)
     {
         for (int j = 0; j < col-2; j++)
@@ -23,5 +25,14 @@ int main()
     for (int i = 0; i < rc; i++)
     {
         printf("%d ",result[i]);
-    }  
+    }
+
+    for (int i = 0; i < rc; i++)
+    {
+        if (result[0] < result[i])
+        {
+            result[0] = result[i];
+        }
+    }
+    printf("\nMaximum sum is: %d", result[0]);
 }
